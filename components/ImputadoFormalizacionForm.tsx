982d7242998437d0839b6d7c011b859ce38d5099
frutillitaaaa <daniela.castro02@alumnos.ucn.cl>
2024-12-26 16:23:19 -0300

    FORM edicion imputado en causa

--- components/ImputadoFormalizacionForm.tsx
+++ components/ImputadoFormalizacionForm.tsx
@@ -8,9 +8,17 @@ import {
     DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
-import { SquarePen } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar"
+import {SquarePen } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+import { z } from "zod"
+import { cn } from "@/lib/utils"
+import { Switch } from "@/components/ui/switch"
+import { Label } from "@/components/ui/label"
 import {
   Form,
   FormControl,
@@ -20,6 +28,11 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 interface ImputadoFormalizacionProps {
     causaId: number,
@@ -29,7 +42,11 @@ interface ImputadoFormalizacionProps {
 const ImputadoFormalizacionForm :React.FC<ImputadoFormalizacionProps>= ({causaId, imputadoId}) => {
     const form = useForm({
         defaultValues: {
-          plazo: "",
+          estaFormalizado: "",
+          fechaFormalizacion: "",
+          medidaCautelar: "",
+          plazo: ""
+
         },
       });
       const { handleSubmit, control } = form;
@@ -64,16 +81,74 @@ const ImputadoFormalizacionForm :React.FC<ImputadoFormalizacionProps>= ({causaId
             <DialogHeader>
               <DialogTitle>Editar Plazo</DialogTitle>
               <DialogDescription>
+              <Switch id="estaFormalizado" />
+              <Label htmlFor="estaFormalizado">Formalizado</Label>
                 <FormProvider {...form}>
                   <form onSubmit={handleSubmit(updatePlazo)}>
+                    <FormField
+                      control={control}
+                      name="fechaFormalizacion"
+                      render={({ field }) => (
+                        <FormItem className="flex flex-col">
+                          <FormLabel>Fecha Formalización</FormLabel>
+                          <Popover>
+                            <PopoverTrigger asChild>
+                              <FormControl>
+                                <Button
+                                  variant={"outline"}
+                                  className={cn(
+                                    "w-[240px] pl-3 text-left font-normal",
+                                    !field.value && "text-muted-foreground"
+                                  )}
+                                >
+                                  {field.value ? (
+                                    format(field.value, "PPP")
+                                  ) : (
+                                    <span>Seleccione una fecha</span>
+                                  )}
+                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                              </FormControl>
+                            </PopoverTrigger>
+                            <PopoverContent className="w-auto p-0" align="start">
+                              <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                disabled={(date) =>
+                                  date > new Date() || date < new Date("1900-01-01")
+                                }
+                                initialFocus
+                              />
+                            </PopoverContent>
+                          </Popover>
+                        </FormItem>
+                      )}
+                    />
                     <FormField
                       control={control}
                       name="plazo"
                       render={({ field }) => (
                         <FormItem>
+                          <FormLabel>Plazo</FormLabel>
                           <FormControl>
                             <Input
                               placeholder="Ingrese la cantidad de días"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={control}
+                      name="medidaCautelar"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Medida Cautelar</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="Ingrese la cautelar"
                               {...field}
                             />
                           </FormControl>
