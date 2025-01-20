e87ea405f8441306748341ea2be514d4bf6c0ecf
frutillitaaaa <daniela.castro02@alumnos.ucn.cl>
2025-01-20 14:35:19 -0300

    creación api causas-crimen organizado y actualización esquema prisma

---
+++ app/api/causas-crimenorganizado/[id]/route.ts
@@ -0,0 +1,89 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+
+interface Props {
+    params: { id: string };
+  }
+
+export async function GET(
+    req: Request, 
+    { params }: {params: { id: string} }
+) {
+    try {
+        const causaId = parseInt(params.id);
+
+        if(!causaId || isNaN(causaId)) {
+            return NextResponse.json(
+                { error: 'ID de causa inválido' },
+                { status: 400}
+            );
+        }
+
+        const causaCrimenOrg = await prisma.causasCrimenOrganizado.findMany({
+            where: { causaId: causaId}, 
+            include: {
+                parametro: true
+            }
+            
+        });
+        console.log(causaCrimenOrg);
+
+        if(!causaCrimenOrg) {
+            return NextResponse.json(
+                { error: 'Datos no encontrados' },
+                { status: 404 }
+            )
+        }
+
+        
+
+        return NextResponse.json(causaCrimenOrg);
+    } catch (error) {
+        console.error('Error fetching causa: ', error);
+        return NextResponse.json(
+            { error: 'Error fetching causa' },
+            { status: 500 }
+        );
+    }
+    
+}
+
+export async function PUT(
+    req: NextRequest,
+    { params }: { params: { id: string }}
+) {
+    try {
+        const data = await req.json();
+        const id = params.id;
+
+        const { causaId, paramId, estado } = data;
+
+        console.log('Received data:', data);
+        console.log('Updating causa with ID:', id);
+
+        const updateData = {
+            paramId: paramId || null,
+            estado: estado || null,
+
+        };
+
+        const updatedCausa = await prisma.causasCrimenOrganizado.update({
+            where: {
+                causaId_parametroId: {
+                    causaId: causaId,
+                    parametroId: paramId
+                }
+            },
+            data: updateData,
+
+
+        })
+    } catch (error) {
+        console.error('Error updating causa: ', error);
+        return NextResponse.json(
+            { error: 'Error interno del servidor' },
+            { status: 500 }
+        );
+    }
+}
+
